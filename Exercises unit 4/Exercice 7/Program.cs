acc875cdeeaea1a369ce5b34af1e03f9e14cb116
cathvafc <miguelangelgomezlope@gmail.com>
2020-12-24 10:30:24 +0100

    Exercice 7

---
+++ Exercises unit 4/Exercice 7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercice_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 1; i<101; i++)
+            {
+
+
+                Console.WriteLine("The value of i is : {0}", i);
+
+            }
+        }
+    }
+}
