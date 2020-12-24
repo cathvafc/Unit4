9f9b388364df94a62d48496b237e4dd7d6ee0098
cathvafc <miguelangelgomezlope@gmail.com>
2020-12-24 11:23:41 +0100

    Exercice 12

---
+++ Exercises unit 4/Exercice 12/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercice_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Miguel Angel";
+
+            Console.WriteLine("Bienvenido {0}.", name);
+        }
+    }
+}
