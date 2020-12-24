2a064f054a237c4ad817c51e26757d2e334c1b35
cathvafc <miguelangelgomezlope@gmail.com>
2020-12-24 11:38:27 +0100

    Exercice 16

---
+++ Exercises unit 4/Exercice 16/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercice_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double ivaPercentage = 0.21;
+
+            Console.WriteLine("Introduce the price of the product: ");
+            double priceWIthoutIva = Convert.ToDouble(Console.ReadLine());
+
+            double iva = ivaPercentage * priceWIthoutIva;
+
+            double priceWithIva = priceWIthoutIva + iva;
+
+            Console.WriteLine("Your product price with IVA is: {0}", priceWithIva);
+        }
+    }
+}
