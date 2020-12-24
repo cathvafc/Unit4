0271c87ccbf3b373e0cfd115c3bfcd84ed0292de
cathvafc <miguelangelgomezlope@gmail.com>
2020-12-24 10:40:03 +0100

    Exercice 9

---
+++ Exercises unit 4/Exercice 9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercice_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("The value of i is: {0}", i);
+
+                }
+            }
+        }
+    }
+}
