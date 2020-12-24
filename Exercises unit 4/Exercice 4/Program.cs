2d52dec3ff22165b787fe17f4432894541af018f
cathvafc <miguelangelgomezlope@gmail.com>
2020-12-24 10:06:41 +0100

    Exercice 4

---
+++ Exercises unit 4/Exercice 4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercice_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert the first number");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Insert the first number");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+        
+            if (firstNumber > secondNumber)
+            {
+                Console.WriteLine("The first number is bigger thant the second one.");
+            }
+            else
+            {
+                Console.WriteLine("The second number is bigger thant the first one.");
+            }
+        }
+    }
+}
