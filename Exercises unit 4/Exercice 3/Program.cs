56d2819ae59a5677757f96b243af47dd10b7b69d
cathvafc <miguelangelgomezlope@gmail.com>
2020-12-24 09:59:41 +0100

    Exercice 3

---
+++ Exercises unit 4/Exercice 3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercice_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, write your name:");
+            string name =  Console.ReadLine();
+
+            Console.WriteLine("Please, write your age:");
+            string age = Console.ReadLine();
+
+            Console.WriteLine("Te llamas {0} y tienes {1} años.", name, age);
+        }
+    }
+}
