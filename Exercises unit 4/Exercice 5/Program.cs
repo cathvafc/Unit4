000ae557cd7086d2334871eb68bd115cdf744c7c
cathvafc <miguelangelgomezlope@gmail.com>
2020-12-24 10:12:17 +0100

    Exercice 5

---
+++ Exercises unit 4/Exercice 5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercice_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, insert the day of the week.");
+            string weekDay = Console.ReadLine();
+
+
+            //Check weekend days in english and spanish.
+
+            if (weekDay == "Sabado" || weekDay == "Domingo" || weekDay == "Saturday" || weekDay == "Sunday" )
+            {
+                Console.WriteLine("{0} is a weekend day",weekDay);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a weekend day", weekDay);
+            }
+        }
+    }
+}
